2dccc027d84224cd24031b6d67d91acb75364131
Estebann31 <esteban.caballero@alu.uclm.es>
2023-04-14 12:13:53 +0200

    Inicio

---
+++ src/MainFrame.java
@@ -0,0 +1,10 @@
+import javax.swing.*;
+
+public class MainFrame {
+    private JButton mostrarHubButton;
+    private JButton apilarButton;
+    private JButton desapilarButton;
+    private JButton mostrarDatosButton;
+    private JButton procedenciaPorPaísButton;
+    private JTextField introduceInformaciónTextField;
+}
