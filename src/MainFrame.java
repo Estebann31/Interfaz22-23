25cf9c2ffe9a5017767bbd19f81f504f43ca9ba6
Estebann31 <esteban.caballero@alu.uclm.es>
2023-04-14 12:52:06 +0200

    nose

--- src/MainFrame.java
+++ src/MainFrame.java
@@ -1,10 +1,40 @@
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainFrame {
+public class MainFrame extends JFrame{
     private JButton mostrarHubButton;
     private JButton apilarButton;
     private JButton desapilarButton;
     private JButton mostrarDatosButton;
     private JButton procedenciaPorPaísButton;
     private JTextField introduceInformaciónTextField;
+    private JPanel main_panel;
+    private JTextArea print;
+    private pais
+
+    public MainFrame ()
+    {
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setTitle("Ejemplo de ventana");
+        this.setSize(700, 500);
+        this.setVisible(true);
+        this.add(main_panel);
+
+        mostrarDatosButton.setVisible(false);
+        mostrarHubButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Barco barco1 = new Barco();
+                print.setText(barco1.toString());
+                mostrarDatosButton.setVisible(true);
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        MainFrame ventana = new MainFrame();
+
+    }
+
 }
